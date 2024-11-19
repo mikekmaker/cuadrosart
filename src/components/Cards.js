11ffea25c8e6778fa5b7e12c5a18cb7e147bce00
MBALVIN <mzenteno@asap-consulting.net>
2024-11-19 19:11:03 -0300

    change images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,35 +11,35 @@ function Cards() {
           <ul className='cards__items'>
             <CardItem
               src='images/img-8.jpg'
-              text='Cancha Premium'
-              label='Confort'
-              path='/services'
+              text='Titulo: xxxxxxxxxxxxxxxxxxx'
+              label='Autor: xxxxxxxxxxxxxxxxxxx'
+              path='/galeria'
             />
             <CardItem
               src='images/img-2.jpg'
-              text='Cancha al Aire Libre'
-              label='Relax'
-              path='/services'
+              text='Titulo: xxxxxxxxxxxxxxxxxxx'
+              label='Autor: xxxxxxxxxxxxxxxxxxx'
+              path='/galeria'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
               src='images/img-3.jpg'
-              text='Cancha con amplias tribunas'
-              label='Emoci&oacute;n'
-              path='/services'
+              text='Titulo: xxxxxxxxxxxxxxxxxxx'
+              label='Autor: xxxxxxxxxxxxxxxxxxx'
+              path='/galeria'
             />
             <CardItem
-              src='images/wpt.jpg'
-              text='Un espacio para torneos y canchas libres todo el a&ntilde;o'
-              label='Comunidad'
-              path='/products'
+              src='images/img-7.jpg'
+              text='Titulo: xxxxxxxxxxxxxxxxxxx'
+              label='Autor: xxxxxxxxxxxxxxxxxxx'
+              path='/galeria'
             />
             <CardItem
               src='images/img-9.jpg'
-              text='Cancha doble para torneos libres de hasta 16 personas'
-              label='Adrenalina'
-              path='/sign-up'
+              text='Titulo: xxxxxxxxxxxxxxxxxxx'
+              label='Autor: xxxxxxxxxxxxxxxxxxx'
+              path='/galeria'
             />
           </ul>
         </div>
